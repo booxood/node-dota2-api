0b0d6cbab0e7d4211825444c63c7a9f5687cd8f1
Liucw <liucw.mail@gmail.com>
2014-11-13 11:59:06 +0800

    Finished all web api

--- README.md
+++ README.md
@@ -25,18 +25,113 @@ Example:
 
 ## API
 
+Methods relating to Dota 2 matches.
+
 ### Dota2Api.create(key)
 Create Dota2Api instance,require a key.(if no, see [here](http://steamcommunity.com/dev/apikey))
 ```javascript
   var da = Dota2Api.create('a key');
 ```
 
-### Dota2Api.prototype.getMatchHistory([options], callback)
-
+### Dota2Api.prototype.getLeagueListing([options], callback)
+Information about DotaTV-supported leagues.
 ```javascript
-  fd.readlines(filePath, function(err, lines){
-      console.log(lines);
-  });
+  da.getLeagueListing({language: 'zh'}, function(err, result){
+    if(!err)
+      console.log(result);
+  })
+```
+
+### Dota2Api.prototype.getLiveLeagueGames(callback)
+A list of in-progress league matches, as well as details of that match as it unfolds.
+```javascript
+  da.getLiveLeagueGames(function(err, result){
+    if(!err)
+      console.log(result);
+  })
+```
+
+### Dota2Api.prototype.getMatchDetails([options], callback)
+Information about a particular match.
+```javascript
+  da.getMatchDetails({match_id: 'xxx'}, function(err, result){
+    if(!err)
+      console.log(result);
+  })
+```
+
+### Dota2Api.prototype.getMatchHistory([options], callback)
+A list of matches, filterable by various parameters.
+```javascript
+  da.getMatchHistory(function(err, result){
+    if(!err)
+      console.log(result);
+  })
+```
+
+### Dota2Api.prototype.getMatchHistoryBySequenceNum([options], callback)
+A list of matches ordered by their sequence num.
+```javascript
+  da.getMatchHistoryBySequenceNum(function(err, result){
+    if(!err)
+      console.log(result);
+  })
+```
+
+### Dota2Api.prototype.getScheduledLeagueGames([options], callback)
+A list of scheduled league games coming up.
+```javascript
+  da.getScheduledLeagueGames(function(err, result){
+    if(!err)
+      console.log(result);
+  })
+```
+
+### Dota2Api.prototype.getTeamInfoByTeamID([options], callback)
+A list of all the teams set up in-game.
+```javascript
+  da.getTeamInfoByTeamID(function(err, result){
+    if(!err)
+      console.log(result);
+  })
+```
+
+### Dota2Api.prototype.getTournamentPlayerStats([options], callback)
+Stats about a particular player within a tournament.
+```javascript
+  da.getTournamentPlayerStats({account_id: 'xxx'}, function(err, result){
+    if(!err)
+      console.log(result);
+  })
+```
+
+Methods relating to Dota 2.
+
+### Dota2Api.prototype.getRarities([options], callback)
+Dota 2 item rarity list.
+```javascript
+  da.getRarities({language: 'zh'}, function(err, result){
+    if(!err)
+      console.log(result);
+  })
+```
+
+### Dota2Api.prototype.getHeroes([options], callback)
+A list of heroes within Dota 2.
+```javascript
+  da.getHeroes({language: 'zh'}, function(err, result){
+    if(!err)
+      console.log(result);
+  })
+```
+
+### Dota2Api.prototype.getTournamentPrizePool([options], callback)
+The current prizepool for specific tournaments.
+```javascript
+  da.getTournamentPrizePool(function(err, result){
+    if(!err)
+      console.log(result);
+  })
 ```
 
 
--- test/dota2-api.js
+++ test/dota2-api.js
@@ -26,8 +26,16 @@ describe('Dota2Api test case', function() {
             da = Dota2Api.create('a key');
         });
 
-        it('should return , getMatchHistory', function(done) {
-            da.getMatchHistory(function(err, result){
+        it('should return , getLeagueListing', function(done) {
+            da.getLeagueListing(function(err, result){
+                should.not.exist(err);
+                should.exist(result);
+                done();
+            });
+        });
+
+        it('should return , getLiveLeagueGames', function(done) {
+            da.getLiveLeagueGames(function(err, result){
                 should.not.exist(err);
                 should.exist(result);
                 done();
@@ -36,6 +44,70 @@ describe('Dota2Api test case', function() {
 
         it('should return , getMatchDetails', function(done) {
             da.getMatchDetails({match_id: 'match id'}, function(err, result){
+                should.not.exist(err);
+                should.exist(result);
+                done();
+            });
+        });
+
+        it('should return , getMatchHistory', function(done) {
+            da.getMatchHistory(function(err, result){
+                should.not.exist(err);
+                should.exist(result);
+                done();
+            });
+        });
+
+        it('should return , getMatchHistoryBySequenceNum', function(done) {
+            da.getMatchHistoryBySequenceNum({start_at_match_seq_num: 'seq num'}, function(err, result){
+                should.not.exist(err);
+                should.exist(result);
+                done();
+            });
+        });
+
+        it('should return , getScheduledLeagueGames', function(done) {
+            da.getScheduledLeagueGames({date_min: '', date_max: ''}, function(err, result){
+                should.not.exist(err);
+                should.exist(result);
+                done();
+            });
+        });
+
+        it('should return , getTeamInfoByTeamID', function(done) {
+            da.getTeamInfoByTeamID(function(err, result){
+                should.not.exist(err);
+                should.exist(result);
+                done();
+            });
+        });
+
+        it('should return , getTournamentPlayerStats', function(done) {
+            da.getTournamentPlayerStats({account_id: 'id'}, function(err, result){
+                should.not.exist(err);
+                should.exist(result);
+                done();
+            });
+        });
+
+        it('should return , getRarities', function(done) {
+            da.getRarities({language: 'zh'}, function(err, result){
+                should.not.exist(err);
+                should.exist(result);
+                done();
+            });
+        });
+
+        it('should return , getHeroes', function(done) {
+            da.getHeroes({language: 'zh'}, function(err, result){
+                should.not.exist(err);
+                should.exist(result);
+                done();
+            });
+        });
+
+        it('should return , getTournamentPrizePool', function(done) {
+            da.getTournamentPrizePool(function(err, result){
                 should.not.exist(err);
                 should.exist(result);
                 done();
